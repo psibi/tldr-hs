afdf9a7ea6fb023f9278023ecc0d639c20bd3e0d
Sibi Prabakaran <sibi@psibi.in>
2017-03-19 03:48:11 +0530

    Initial working code

---
+++ app/Main.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE ExtendedDefaultRules #-}
+
+module Main where
+
+import Tldr
+import Options.Applicative
+import Data.Semigroup ((<>))
+import Control.Monad
+import System.Directory
+import System.FilePath
+import Data.Conduit.Shell hiding (info)
+
+data TldrOpts = TldrOpts
+  { pageName :: String
+  , updateFlag :: Bool
+  } deriving (Show)
+
+tldrDirName :: String
+tldrDirName = ".tldr"
+
+repoHttpsUrl :: String
+repoHttpsUrl = "https://github.com/tldr-pages/tldr.git"
+
+checkDirs :: [String]
+checkDirs = ["common", "linux", "osx"]
+
+tldrInitialized :: IO Bool
+tldrInitialized = do
+  homeDir <- getHomeDirectory
+  let dir1 = homeDir </> tldrDirName
+      dir2 = homeDir </> tldrDirName </> "tldr"
+      pages = homeDir </> tldrDirName </> "tldr" </> "pages"
+  exists <- mapM doesDirectoryExist [dir1, dir2, pages]
+  return $ all (== True) exists
+
+initializeTldrPages :: IO ()
+initializeTldrPages = do
+  initialized <- tldrInitialized
+  if initialized
+    then return ()
+    else do
+      homeDir <- getHomeDirectory
+      run $
+        do mkdir (homeDir </> tldrDirName)
+           cd (homeDir </> tldrDirName)
+           git "clone" repoHttpsUrl
+
+updateTldrPages :: IO ()
+updateTldrPages = do
+  homeDir <- getHomeDirectory
+  let repoDir = homeDir </> tldrDirName <> "tldr"
+  run $
+    do cd repoDir
+       git "pull" ["origin", "master"]
+
+tldrParserInfo :: ParserInfo TldrOpts
+tldrParserInfo =
+  info
+    (helper <*> versionOption <*> programOptions)
+    (fullDesc <> progDesc "tldr Client program" <>
+     header "tldr - Simplified and community-driven man pages")
+  where
+    versionOption = infoOption "0.1" (long "version" <> help "Show version")
+    programOptions :: Parser TldrOpts
+    programOptions =
+      TldrOpts <$>
+      (strArgument (metavar "COMMAND" <> help "name of the command")) <*>
+      (switch (long "update" <> help "Update tldr pages"))
+
+pageExists :: FilePath -> IO (Maybe FilePath)
+pageExists fname = do
+  exists <- doesFileExist fname
+  if exists
+    then return $ Just fname
+    else return Nothing
+
+getPagePath :: String -> IO (Maybe FilePath)
+getPagePath page = do
+  homeDir <- getHomeDirectory
+  let pageDir = homeDir </> tldrDirName </> "tldr" </> "pages"
+      x@(f1:f2:f3:[]) = map (\x -> pageDir </> x </> page <.> "md") checkDirs
+  pageExists f1 <|> pageExists f2 <|> pageExists f3
+
+main :: IO ()
+main = do
+  initializeTldrPages
+  opts <- execParser tldrParserInfo
+  when (updateFlag opts) updateTldrPages
+  fname <- getPagePath (pageName opts)
+  case fname of
+    Nothing -> putStrLn ("No tldr entry for " <> (pageName opts))
+    Just fpath -> renderPage fpath
---
+++ src/Tldr.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Tldr
+  ( parsePage
+  , renderPage
+  , ConsoleSetting(..)
+  , defConsoleSetting
+  , headingSetting
+  , toSGR
+  , renderNode
+  , changeConsoleSetting
+  ) where
+
+import Data.Text
+import qualified Data.Text.IO as TIO
+import CMark
+import System.Console.ANSI
+import Data.Monoid ((<>))
+
+data ConsoleSetting = ConsoleSetting
+  { italic :: Bool
+  , underline :: Underlining
+  , blink :: BlinkSpeed
+  , fgIntensity :: ColorIntensity
+  , fgColor :: Color
+  , bgIntensity :: ColorIntensity
+  , bgColor :: Color
+  , consoleIntensity :: ConsoleIntensity
+  }
+
+defConsoleSetting :: ConsoleSetting
+defConsoleSetting =
+  ConsoleSetting
+  { italic = False
+  , underline = NoUnderline
+  , blink = NoBlink
+  , fgIntensity = Dull
+  , fgColor = White
+  , bgIntensity = Dull
+  , bgColor = Black
+  , consoleIntensity = NormalIntensity
+  }
+
+headingSetting :: ConsoleSetting
+headingSetting =
+  defConsoleSetting
+  { consoleIntensity = BoldIntensity
+  }
+
+toSGR :: ConsoleSetting -> [SGR]
+toSGR cons =
+  [ SetItalicized (italic cons)
+  , SetConsoleIntensity (consoleIntensity cons)
+  , SetUnderlining (underline cons)
+  , SetBlinkSpeed (blink cons)
+  , SetColor Foreground (fgIntensity cons) (fgColor cons)
+  , SetColor Background (bgIntensity cons) (bgColor cons)
+  ]
+
+renderNode :: NodeType -> IO ()
+renderNode (TEXT txt) = TIO.putStrLn txt
+renderNode (HTML_BLOCK txt) = TIO.putStrLn txt
+renderNode (CODE_BLOCK _ txt) = TIO.putStrLn txt
+renderNode (HTML_INLINE txt) = TIO.putStrLn txt
+renderNode (CODE txt) = TIO.putStrLn ("   " <> txt)
+renderNode LINEBREAK = TIO.putStrLn ""
+renderNode (LIST _) = TIO.putStrLn "" >> TIO.putStr " - "
+renderNode _ = return ()
+
+changeConsoleSetting :: NodeType -> IO ()
+changeConsoleSetting (HEADING _) = setSGR $ toSGR headingSetting
+changeConsoleSetting BLOCK_QUOTE = setSGR $ toSGR headingSetting
+changeConsoleSetting ITEM =
+  setSGR $
+  toSGR $
+  defConsoleSetting
+  { fgColor = Green
+  }
+changeConsoleSetting (CODE _) =
+  setSGR $
+  toSGR $
+  defConsoleSetting
+  { fgColor = Yellow
+  }
+changeConsoleSetting _ = return ()
+
+handleNode :: Node -> IO ()
+handleNode (Node _ ntype xs) = do
+  changeConsoleSetting ntype
+  renderNode ntype
+  mapM_ (\(Node _ ntype' ns) -> renderNode ntype' >> mapM_ handleNode ns) xs
+
+parsePage :: FilePath -> IO Node
+parsePage fname = do
+  page <- TIO.readFile fname
+  let node = commonmarkToNode [] page
+  return node
+
+renderPage :: FilePath -> IO ()
+renderPage fname = do
+  node <- parsePage fname
+  handleNode node
