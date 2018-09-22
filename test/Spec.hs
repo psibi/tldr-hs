d73670c95339d9943715ee7d8b614b6eb7cdd276
Sibi Prabakaran <sibi@psibi.in>
2018-09-22 20:32:37 +0530

    Add basic tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,27 @@
+import Tldr
+import Test.Tasty
+import Test.Tasty.Golden (goldenVsFile)
+import System.IO (withBinaryFile, IOMode(..))
+
+tests :: TestTree
+tests = testGroup "tldr Tests" [goldenTests]
+
+goldenTests :: TestTree
+goldenTests = testGroup "Golden tests" [gtests]
+
+renderPageToFile :: FilePath -> FilePath -> IO ()
+renderPageToFile mdfile opfile = do
+  withBinaryFile opfile WriteMode (\handle -> renderPage mdfile handle)
+
+gtests :: TestTree
+gtests = testGroup "(render test)" 
+         [
+          goldenVsFile 
+          "ls test" 
+          "test/data/ls.golden"
+          "test/data/ls.output"
+          (renderPageToFile "test/data/ls.md" "test/data/ls.output")
+         ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
