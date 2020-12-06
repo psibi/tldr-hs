62e87db2d63c9197d2f950bc4379c7004984f47d
Sibi Prabakaran <sibi@psibi.in>
2020-12-06 11:37:20 +0530

    Fix musl

--- .azure/build_release.sh
+++ .azure/build_release.sh
@@ -4,6 +4,9 @@ set -eux
 
 export PATH=$HOME/.local/bin:$PATH
 
+# Reg: BUILD_BINARIESDIRECTORY
+# https://docs.microsoft.com/en-us/azure/devops/pipelines/tasks/utility/archive-files?view=azure-devops
+
 rm -rf "${BUILD_BINARIESDIRECTORY}"
 mkdir "${BUILD_BINARIESDIRECTORY}"
 
